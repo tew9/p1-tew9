86a8bda54fda92df4490d0d3d666362da448186e
tew9 <tangogatdet76@gmail.com>
2020-03-22 01:34:47 -0500

    fix price

--- PizzaBox.Client/Controllers/PizzaController.cs
+++ PizzaBox.Client/Controllers/PizzaController.cs
@@ -11,16 +11,8 @@ namespace PizzaBox.Client.Controllers
     private static List<PizzaViewModel> selection = new List<PizzaViewModel>();
     [HttpGet]
     public IActionResult Order(string store)
-    { 
-      // ViewData["pizza"] =pm.Pizz();
-     // string name = "";
-      PizzaViewModel pm = new PizzaViewModel();
-      // foreach(var s in store){
-      //   name = s.Name;
-      // }
-      ViewData["pizza"] = store;
-      
-      return View(pm);
+    {       
+      return View(new PizzaViewModel());
     }
 
     [HttpPost]
@@ -30,17 +22,10 @@ namespace PizzaBox.Client.Controllers
       return View("OrderDetails", selection);
     }
 
-    // [HttpGet]
     public IActionResult OrderDetails(int Id)
     {
       selection.RemoveAt(Id);
       return View("OrderDetails", selection);
     }
-    // public IActionResult OrderDetails(PizzaViewModel model)
-    // {
-    //   // model.ToList()
-    //   selection.Add(model);
-    //   return View("OrderDetails", selection);
-    // }
   }
 }
